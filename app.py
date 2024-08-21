1a7bae7d73d48e9297d8c252ccd82c9e25b6d12e
sadhu25shree <159885096+sadhu25shree@users.noreply.github.com>
2024-08-21 21:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 
 import streamlit as st
-import google
-
+import google.generativeai as genai
 
 st.title("welcome to gemini chat")
 
-#generativeai as genai
 
 genai.configure(api_key="AIzaSyC-6mgizFNSpgYfq4bDNOIt1TN0PAeVaL8")
 
-text = st.text_input("enter your question")
+text = st.text_input("Enter Your Question")
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
-if st.button("click me"):
+if st.button("Click Me"):
     response = chat.send_message(text)
     st.write(response.text)
